1c6dbae0d09d059c9403bed942a9b38884c05fb1
Czar-250 <manziczar@gmail.com>
2022-09-08 12:40:15 -0700

    Top of the stack

---
+++ add.c
@@ -0,0 +1,82 @@
+#include "monty.h"
+#include "lists.h"
+
+/**
+ * free_all - custom memory handling function
+ *
+ *
+ * @all: flag to indicate what to free
+ */
+void free_all(int all)
+{
+	if (data.line)
+	{
+		free(data.line);
+		data.line = NULL;
+		free_everything(data.words);
+		data.words = NULL;
+	}
+
+	if (all)
+	{
+		if (data.stack)
+		{
+			free_dlistint(data.stack);
+			data.stack = NULL;
+		}
+		if (data.fptr)
+		{
+			fclose(data.fptr);
+			data.fptr = NULL;
+		}
+	}
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"add.c" 0L, 0C                                                           0,0-1         All
+
